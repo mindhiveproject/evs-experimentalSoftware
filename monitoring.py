b63d17b000d6a234150377f0b63dd33f08789281
Franck Porteous <franck.porteous@protonmail.com>
2026-05-28 12:08:20 -0400

    (test) plotting liveamp

--- monitoring.py
+++ monitoring.py
@@ -1,32 +1,32 @@
 #%%
 from LabRecorder_Viewer import XDFExplorer
-explorer = XDFExplorer("./data/DY_001.xdf")
+explorer = XDFExplorer("./data/DY_002.xdf")
 #%%
-explorer.plot_streams_enhanced(
-    indices=[43],
-    channels_per_stream=[list(range(19))],
-    labels=["BrainVision RDA"],
-    time_range=(5, 15),
-    clip_std_outliers=True,
-)
+# explorer.plot_streams_enhanced(
+#     indices=[43],
+#     channels_per_stream=[list(range(19))],
+#     labels=["BrainVision RDA"],
+#     time_range=(5, 15),
+#     clip_std_outliers=True,
+# )
 # %%
 explorer.plot_channels_grouped(
-    idx=43,
-    channels=list(range(19)),   # optional; default = all channels
-    channels_per_subplot=1,     # default
-    label="BrainVision RDA",
-    time_range=(600, 660),
+    idx=13,
+    channels=list(range(19)),
+    channels_per_subplot=1,
+    label="V-Amp",
+    time_range=(600, 614),
     clip_std_outliers=True,
     show_markers=True,
 )
 
 # %%
 explorer.plot_channels_grouped(
-    idx=11,
-    channels=list(range(19)),   # optional; default = all channels
-    channels_per_subplot=1,     # default
-    label="BrainVision RDA",
-    time_range=(600, 660),
+    idx=0,
+    channels=list(range(20)),
+    channels_per_subplot=1,
+    label="LiveAmp",
+    # time_range=(600, 614),
     clip_std_outliers=True,
     show_markers=True,
 )
